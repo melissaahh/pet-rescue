3a61667f7e5e538eb1d30aca00bbfb1d6920cc76
Melissa Harrison <78873771+melissaahh@users.noreply.github.com>
2024-05-23 14:35:17 -0400

    Added declaration of objects

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const createPet = function (name, species) {
         sleep: function () {
             console.log(`${this.name} needs a nap. Zzz...`);
             this.isTired = 1;
-        }
+        },
         play: function () {
             if (this.isTired === 10) {
                 console.log(`Too tired to play.`)
@@ -34,8 +34,36 @@ const francine = createPet("Francine", "turtle");
 
 // console.log (sora, clover, baxter, cleo, francine);
 
-clover.sleep();
-baxter.play();
+// clover.sleep();
+// baxter.play();
 
-console.log(clover, baxter);
+// console.log(clover, baxter);
 
+clover.isTired = 8;
+francine.isTired = 9;
+
+const allPets = [
+    sora,
+    clover,
+    baxter,
+    cleo,
+    francine
+];
+
+const showPets = function (petArray) {
+    pets.innerHTML = "";
+
+    for (let pet of petArray) {
+        let status = "ready to play!";
+        if (pet.isTired >= 7) {
+            status = "sleeping.";
+        }  
+         const li = document.createElement("li");
+         li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
+         pets.append(li);
+      }
+}
+
+statusButton.addEventListener ("click", function () {
+    showPets(allPets);
+}); 
